69086f9600efc5147e944f1de40c82005790cbe2
Mötz Jensen <motz@e-s.dk>
2019-05-16 21:56:56 +0200

    Clearing old files

--- internal/sql/set-recurringbatchjobstartdate.sql
+++
@@ -1,5 +0,0 @@
-update dbo.BatchJob
-set ORIGSTARTDATETIME = DATEFROMPARTS(year(getdate()),01,01)
-,RECVERSION = RECVERSION +1 output deleted.CAPTION
-where caption like 'Batch job for %' + @Name + '%'
-and [status] = 1
