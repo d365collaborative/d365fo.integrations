211126cb904e147569eaedb8cbf1bd94d96ea31f
Mötz Jensen <motz@e-s.dk>
2019-05-17 19:57:20 +0200

    Update enable-d365exception.ps1

--- d365fo.integrations/functions/enable-d365exception.ps1
+++ d365fo.integrations/functions/enable-d365exception.ps1
@@ -1,2 +1,20 @@
-﻿
-<#
+﻿<#
+.SYNOPSIS
+Enable exceptions to be thrown
+
+.DESCRIPTION
+Change the default exception behavior of the module to support throwing exceptions
+
+.EXAMPLE
+PS C:\> 
+
+.NOTES
+General notes
+#>
+
+function Enable-d365Exception {
+    [CmdletBinding()]
+    param ()
+
+    $PSDefaultParameterValues['*:EnableException'] = $true
+}
