2fd66d8c145770a4dddd80a713e3a9d558231a7a
Mötz Jensen <motz@e-s.dk>
2019-05-18 13:11:58 +0200

    import

--- d365fo.integrations/functions/get-d365entityviaodata.ps1
+++ d365fo.integrations/functions/get-d365entityviaodata.ps1
@@ -1,15 +1,22 @@
 ﻿
 
 function Get-D365ODataEntityData {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = "Default")]
     [OutputType()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $true, ParameterSetName = "Specific")]
         [Alias('EntityName')]
         [string] $Name,
 
+        [Parameter(Mandatory = $true, ParameterSetName = "Default", ValueFromPipelineByPropertyName = $true)]
+        [Alias('CollectionName')]
+        [string] $EntitySetName,
+
         [Parameter(Mandatory = $false)]
         [string] $ODataQuery,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $CrossCompany,
 
         [Parameter(Mandatory = $false)]
         [Alias('$AADGuid')]
@@ -27,36 +34,52 @@ function Get-D365ODataEntityData {
 
     )
 
-    $bearerParms = @{
-        Resource     = $URL
-        ClientId     = $ClientId
-        ClientSecret = $ClientSecret
+    begin {
+        $bearerParms = @{
+            Resource     = $URL
+            ClientId     = $ClientId
+            ClientSecret = $ClientSecret
+        }
+
+        $bearerParms.AuthProviderUri = "https://login.microsoftonline.com/$Tenant/oauth2/token"
+
+        $bearer = Invoke-ClientCredentialsGrant @bearerParms | Get-BearerToken
+
+        $headerParms = @{
+            URL         = $URL
+            BearerToken = $bearer
+        }
+
+        $headers = New-AuthorizationHeaderBearerToken @headerParms
     }
 
-    $bearerParms.AuthProviderUri = "https://login.microsoftonline.com/$Tenant/oauth2/token"
+    process {
+        
+        [System.UriBuilder] $odataEndpoint = $URL
+        
+        if (-not ([string]::IsNullOrEmpty($Name))) {
+            $odataEndpoint.Path = "data/$Name"
+        }
+        else {
+            $odataEndpoint.Path = "data/$EntitySetName"
+        }
 
-    $bearer = Invoke-ClientCredentialsGrant @bearerParms | Get-BearerToken
+        if (-not ([string]::IsNullOrEmpty($ODataQuery))) {
+            $odataEndpoint.Query = "$ODataQuery"
+        }
+        
+        if($CrossCompany){
+            $odataEndpoint.Query = $($odataEndpoint.Query + "&cross-company=true").Replace("?", "")
+        }
 
-    $headerParms = @{
-        URL         = $URL
-        BearerToken = $bearer
-    }
-
-    $headers = New-AuthorizationHeaderBearerToken @headerParms
-
-    [System.UriBuilder] $odataEndpoint = $URL
-    $odataEndpoint.Path = "data/$Name"
-
-    if (-not ([string]::IsNullOrEmpty($ODataQuery))) {
-        $odataEndpoint.Query = "$ODataQuery"
-    }
-
-    try {
-        Invoke-RestMethod -Method Get -Uri $odataEndpoint.Uri.AbsoluteUri -Headers $headers -ContentType 'application/json'
-    }
-    catch {
-        Write-PSFMessage -Level Host -Message "Something went wrong while trying to send a message to the users." -Exception $PSItem.Exception
-        Stop-PSFFunction -Message "Stopping because of errors."
-        return
+        try {
+            Write-PSFMessage -Level Verbose -Message "Executing http request against the OData endpoint." -Target $($odataEndpoint.Uri.AbsoluteUri)
+            Invoke-RestMethod -Method Get -Uri $odataEndpoint.Uri.AbsoluteUri -Headers $headers -ContentType 'application/json'
+        }
+        catch {
+            Write-PSFMessage -Level Host -Message "Something went wrong while trying to send a message to the users." -Exception $PSItem.Exception
+            Stop-PSFFunction -Message "Stopping because of errors."
+            return
+        }
     }
 }
--- d365fo.integrations/functions/get-d365odatapublicentity.ps1
+++ d365fo.integrations/functions/get-d365odatapublicentity.ps1
@@ -30,42 +30,50 @@ function Get-D365ODataPublicEntity {
 
     )
 
-    $bearerParms = @{
-        Resource     = $URL
-        ClientId     = $ClientId
-        ClientSecret = $ClientSecret
+
+    begin {
+        
+        $bearerParms = @{
+            Resource     = $URL
+            ClientId     = $ClientId
+            ClientSecret = $ClientSecret
+        }
+
+        $bearerParms.AuthProviderUri = "https://login.microsoftonline.com/$Tenant/oauth2/token"
+
+        $bearer = Invoke-ClientCredentialsGrant @bearerParms | Get-BearerToken
+
+        $headerParms = @{
+            URL         = $URL
+            BearerToken = $bearer
+        }
+
+        $headers = New-AuthorizationHeaderBearerToken @headerParms
+
+        [System.UriBuilder] $odataEndpoint = $URL
+        $odataEndpoint.Path = "metadata/PublicEntities"
     }
 
-    $bearerParms.AuthProviderUri = "https://login.microsoftonline.com/$Tenant/oauth2/token"
+    process {
+        $odataEndpoint.Query = ""
 
-    $bearer = Invoke-ClientCredentialsGrant @bearerParms | Get-BearerToken
+        if (-not ([string]::IsNullOrEmpty($ODataQuery))) {
+            $odataEndpoint.Query = "$ODataQuery"
+        }
+        elseif (-not ([string]::IsNullOrEmpty($EntityName))) {
+            $odataEndpoint.Query = "`$filter=tolower(Name) eq tolower('$EntityName') or tolower(EntitySetName) eq tolower('$EntityName')"
+        }
+        elseif (-not ([string]::IsNullOrEmpty($EntityNameContains))) {
+            $odataEndpoint.Query = "`$filter=contains(tolower(Name), tolower('$EntityNameContains')) or contains(tolower(EntitySetName), tolower('$EntityNameContains'))"
+        }
 
-    $headerParms = @{
-        URL         = $URL
-        BearerToken = $bearer
-    }
-
-    $headers = New-AuthorizationHeaderBearerToken @headerParms
-
-    [System.UriBuilder] $odataEndpoint = $URL
-    $odataEndpoint.Path = "metadata/PublicEntities"
-
-    if (-not ([string]::IsNullOrEmpty($ODataQuery))) {
-        $odataEndpoint.Query = "$ODataQuery"
-    }
-    elseif(-not ([string]::IsNullOrEmpty($EntityName))) {
-        $odataEndpoint.Query = "`$filter=tolower(Name) eq tolower('$EntityName') or tolower(EntitySetName) eq tolower('$EntityName')"
-    }
-    elseif(-not ([string]::IsNullOrEmpty($EntityNameContains))) {
-        $odataEndpoint.Query = "`$filter=contains(tolower(Name), tolower('$EntityNameContains')) or contains(tolower(EntitySetName), tolower('$EntityNameContains'))"
-    }
-
-    try {
-        Invoke-RestMethod -Method Get -Uri $odataEndpoint.Uri.AbsoluteUri -Headers $headers -ContentType 'application/json'
-    }
-    catch {
-        Write-PSFMessage -Level Host -Message "Something went wrong while trying to send a message to the users." -Exception $PSItem.Exception
-        Stop-PSFFunction -Message "Stopping because of errors."
-        return
+        try {
+            Invoke-RestMethod -Method Get -Uri $odataEndpoint.Uri.AbsoluteUri -Headers $headers -ContentType 'application/json'
+        }
+        catch {
+            Write-PSFMessage -Level Host -Message "Something went wrong while trying to send a message to the users." -Exception $PSItem.Exception
+            Stop-PSFFunction -Message "Stopping because of errors."
+            return
+        }
     }
 }
