b75ea2700ee3f4d85f8eef740ddef05ed067d94a
Mötz Jensen <motz@e-s.dk>
2019-05-19 19:31:26 +0200

    Import

--- d365fo.integrations/d365fo.integrations.psd1
+++ d365fo.integrations/d365fo.integrations.psd1
@@ -52,7 +52,8 @@
         ,	'Get-D365ODataPublicEntity'
         
         ,   'Import-D365ODataEntity'
-		
+        
+        ,   'Remove-D365ODataEntity'
         ,	'Set-D365ActiveODataConfig'
 
     )
---
+++ d365fo.integrations/functions/remove-d365odataentity.ps1
@@ -0,0 +1,133 @@
+﻿<#
+.SYNOPSIS
+Import a Data Entity into Dynamics 365 Finance & Operations
+
+.DESCRIPTION
+Imports a Data Entity, defined as a json payload, using the OData endpoint of the Dynamics 365 Finance & Operations
+
+.PARAMETER EntityName
+Name of the Data Entity you want to work against
+
+The parameter is Case Sensitive, because the OData endpoint in D365FO is Case Sensitive
+
+Remember that most Data Entities in a D365FO environment is named by its singular name, but most be retrieve using the plural name
+
+E.g. The version 3 of the customers Data Entity is named CustomerV3, but can only be retrieving using CustomersV3
+
+Look at the Get-D365ODataPublicEntity cmdlet to help you obtain the correct name
+
+.PARAMETER Payload
+The entire string contain the json object that you want to import into the D365FO environment
+
+.PARAMETER CrossCompany
+Instruct the cmdlet / function to ensure the request against the OData endpoint will work across all companies
+
+    .PARAMETER Tenant
+        Azure Active Directory (AAD) tenant id (Guid) that the D365FO environment is connected to, that you want to access through OData
+        
+    .PARAMETER URL
+        URL / URI for the D365FO environment you want to access through OData
+        
+    .PARAMETER ClientId
+        The ClientId obtained from the Azure Portal when you created a Registered Application
+        
+    .PARAMETER ClientSecret
+        The ClientSecret obtained from the Azure Portal when you created a Registered Application
+
+    .PARAMETER EnableException
+        This parameters disables user-friendly warnings and enables the throwing of exceptions
+        This is less user friendly, but allows catching exceptions in calling scripts
+
+.EXAMPLE
+PS C:\> Import-D365ODataEntity -EntityName "ExchangeRates" -Json '{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-03T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}'
+
+.NOTES
+Tags: OData, Data, Entity, Import, Upload
+
+Author: Mötz Jensen (@Splaxi)
+#>
+
+function Remove-D365ODataEntity {
+    [CmdletBinding()]
+    [OutputType()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $EntityName,
+
+        [Parameter(Mandatory = $true)]
+        [string[]] $EntityKey,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $CrossCompany,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('$AADGuid')]
+        [string] $Tenant = $Script:ODataTenant,
+
+        [Parameter(Mandatory = $false)]
+        [Alias('URI')]
+        [string] $URL = $Script:ODataUrl,
+
+        [Parameter(Mandatory = $false)]
+        [string] $ClientId = $Script:ODataClientId,
+
+        [Parameter(Mandatory = $false)]
+        [string] $ClientSecret = $Script:ODataClientSecret,
+
+        [switch] $EnableException
+
+    )
+
+    begin {
+        $bearerParms = @{
+            Resource     = $URL
+            ClientId     = $ClientId
+            ClientSecret = $ClientSecret
+        }
+
+        $bearerParms.AuthProviderUri = "https://login.microsoftonline.com/$Tenant/oauth2/token"
+
+        $bearer = Invoke-ClientCredentialsGrant @bearerParms | Get-BearerToken
+
+        $headerParms = @{
+            URL         = $URL
+            BearerToken = $bearer
+        }
+
+        $headers = New-AuthorizationHeaderBearerToken @headerParms
+    }
+
+    process {
+        $keyBuilder = [System.Text.StringBuilder]::new()
+
+        $null = $keyBuilder.Append("(")
+
+        foreach ($item in $EntityKey) {
+            $null = $keyBuilder.Append("$item,")
+        }
+        $keyBuilder.Length--
+        $null = $keyBuilder.Append(")")
+
+        Write-PSFMessage -Level Verbose -Message "Working against $EntityName" -Target $entityName
+        
+        [System.UriBuilder] $odataEndpoint = $URL
+
+        $odataEndpoint.Path = "data/$EntityName$($keyBuilder.ToString())"
+
+        if ($CrossCompany) {
+            $odataEndpoint.Query = $($odataEndpoint.Query + "&cross-company=true").Replace("?", "")
+        }
+
+        try {
+            Write-PSFMessage -Level Verbose -Message "Executing http request against the OData endpoint." -Target $($odataEndpoint.Uri.AbsoluteUri)
+            Invoke-RestMethod -Method DELETE -Uri $odataEndpoint.Uri.AbsoluteUri -Headers $headers -ContentType 'application/json'
+        }
+        catch {
+            $messageString = $((ConvertFrom-Json $_).Error.InnerError | ConvertTo-Json)
+            Write-PSFMessage -Level Host -Message $messageString -Exception $PSItem.Exception -Target $EntityName
+            Stop-PSFFunction -Message "Stopping because of errors." -Exception $([System.Exception]::new($messageString)) -ErrorRecord $_
+            return
+        }
+
+    }
+}
