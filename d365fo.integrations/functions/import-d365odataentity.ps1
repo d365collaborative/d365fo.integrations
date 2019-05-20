2fc515e5e0db1f8883979993931b0f105b867a9c
Mötz Jensen <motz@e-s.dk>
2019-05-20 10:07:39 +0200

    import

--- d365fo.integrations/functions/import-d365odataentity.ps1
+++ d365fo.integrations/functions/import-d365odataentity.ps1
@@ -19,6 +19,8 @@ Look at the Get-D365ODataPublicEntity cmdlet to help you obtain the correct name
 .PARAMETER Payload
 The entire string contain the json object that you want to import into the D365FO environment
 
+Remember that json is text based and can use either single quotes (') or double quotes (") as the text qualifier, so you might need to escape the different quotes in your payload before passing it in
+
 .PARAMETER CrossCompany
 Instruct the cmdlet / function to ensure the request against the OData endpoint will work across all companies
 
@@ -39,7 +41,20 @@ Instruct the cmdlet / function to ensure the request against the OData endpoint 
         This is less user friendly, but allows catching exceptions in calling scripts
 
 .EXAMPLE
-PS C:\> Import-D365ODataEntity -EntityName "ExchangeRates" -Json '{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-03T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}'
+PS C:\> Import-D365ODataEntity -EntityName "ExchangeRates" -Payload '{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-03T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}'
+
+This will import a Data Entity into Dynamics 365 Finance & Operations using the OData endpoint.
+The EntityName used for the import is ExchangeRates.
+The Payload is a valid json string, containing all the needed properties.
+
+.EXAMPLE
+PS C:\> $Payload = '{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-03T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}'
+PS C:\> Import-D365ODataEntity -EntityName "ExchangeRates" -Payload $Payload
+
+This will import a Data Entity into Dynamics 365 Finance & Operations using the OData endpoint.
+First the desired json data is put into the $Payload variable.
+The EntityName used for the import is ExchangeRates.
+The $Payload variable is passed to the cmdlet.
 
 .NOTES
 Tags: OData, Data, Entity, Import, Upload
--- d365fo.integrations/functions/import-d365odataentitybatchmode.ps1
+++ d365fo.integrations/functions/import-d365odataentitybatchmode.ps1
@@ -1,4 +1,70 @@
-﻿function Import-D365ODataEntityBatchMode {
+﻿<#
+.SYNOPSIS
+Import a set of Data Entities into Dynamics 365 Finance & Operations
+
+.DESCRIPTION
+Imports a set of Data Entities, defined as a json payloads, using the OData endpoint of the Dynamics 365 Finance & Operations
+
+The entire payload will be batched into a single request against the OData endpoint
+
+.PARAMETER EntityName
+Name of the Data Entity you want to work against
+
+The parameter is Case Sensitive, because the OData endpoint in D365FO is Case Sensitive
+
+Remember that most Data Entities in a D365FO environment is named by its singular name, but most be retrieve using the plural name
+
+E.g. The version 3 of the customers Data Entity is named CustomerV3, but can only be retrieving using CustomersV3
+
+Look at the Get-D365ODataPublicEntity cmdlet to help you obtain the correct name
+
+.PARAMETER Payload
+The entire string contain the json objects that you want to import into the D365FO environment
+
+Payload supports multiple json objects, that needs to be batched together
+
+.PARAMETER CrossCompany
+Instruct the cmdlet / function to ensure the request against the OData endpoint will work across all companies
+
+    .PARAMETER Tenant
+        Azure Active Directory (AAD) tenant id (Guid) that the D365FO environment is connected to, that you want to access through OData
+        
+    .PARAMETER URL
+        URL / URI for the D365FO environment you want to access through OData
+        
+    .PARAMETER ClientId
+        The ClientId obtained from the Azure Portal when you created a Registered Application
+        
+    .PARAMETER ClientSecret
+        The ClientSecret obtained from the Azure Portal when you created a Registered Application
+
+    .PARAMETER EnableException
+        This parameters disables user-friendly warnings and enables the throwing of exceptions
+        This is less user friendly, but allows catching exceptions in calling scripts
+
+.EXAMPLE
+PS C:\> Import-D365ODataEntityBatchMode -EntityName "ExchangeRates" -Payload '{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-03T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}','{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-04T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}'
+
+This will import a set of Data Entities into Dynamics 365 Finance & Operations using the OData endpoint.
+The EntityName used for the import is ExchangeRates.
+The Payload is an array containing valid json strings, each containing all the needed properties.
+
+.EXAMPLE
+PS C:\> $Payload = '{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-03T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}','{"@odata.type" :"Microsoft.Dynamics.DataEntities.ExchangeRate", "RateTypeName": "TEST", "FromCurrency": "DKK", "ToCurrency": "EUR", "StartDate": "2019-01-04T00:00:00Z", "Rate": 745.10, "ConversionFactor": "Hundred", "RateTypeDescription": "TEST"}'
+PS C:\> Import-D365ODataEntityBatchMode -EntityName "ExchangeRates" -Payload $Payload
+
+This will import a set of Data Entities into Dynamics 365 Finance & Operations using the OData endpoint.
+First the desired json data is put into the $Payload variable.
+The EntityName used for the import is ExchangeRates.
+The $Payload variable is passed to the cmdlet.
+
+.NOTES
+Tags: OData, Data, Entity, Import, Upload
+
+Author: Mötz Jensen (@Splaxi)
+#>
+
+function Import-D365ODataEntityBatchMode {
     [CmdletBinding()]
     [OutputType()]
     param (
