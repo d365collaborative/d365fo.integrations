1a31654bf681070144f7ec6b866e9b43eecd9396
Mötz Jensen <motz@e-s.dk>
2019-05-21 07:07:17 +0200

    import

--- d365fo.integrations/functions/enable-d365exception.ps1
+++ d365fo.integrations/functions/enable-d365exception.ps1
@@ -25,29 +25,3 @@ function Enable-D365Exception {
 
     $PSDefaultParameterValues['*:EnableException'] = $true
 }
-<#
-    .SYNOPSIS
-        Enable exceptions to be thrown
-        
-    .DESCRIPTION
-        Change the default exception behavior of the module to support throwing exceptions
-        
-        Useful when the module is used in an automated fashion, like inside Azure DevOps pipelines and large PowerShell scripts
-        
-    .EXAMPLE
-        PS C:\>Enable-D365Exception
-        
-        This will for the rest of the current PowerShell session make sure that exceptions will be thrown.
-        
-    .NOTES
-        Tags: Exception, Exceptions, Warning, Warnings
-        
-        Author: Mötz Jensen (@Splaxi)
-#>
-
-function Enable-D365Exception {
-    [CmdletBinding()]
-    param ()
-
-    $PSDefaultParameterValues['*:EnableException'] = $true
-}
