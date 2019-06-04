3da687790b3008ae864d869a536474dbb27cbfa5
Mötz Jensen <motz@e-s.dk>
2019-06-04 23:30:51 +0200

    CBH

--- d365fo.integrations/internal/functions/get-dmfpacakgedetails.ps1
+++ d365fo.integrations/internal/functions/get-dmfpacakgedetails.ps1
@@ -1,32 +1,33 @@
-﻿<#
-.SYNOPSIS
-Get DMF Package details
-
-.DESCRIPTION
-Get all the needed details about a DMF package, so you can download it from the Dynamics 365 for Finance & Operations
-
-.PARAMETER JobId
-The GUID from the recurring data job
-
-.PARAMETER AuthenticationToken
-The token value that should be used to authenticate against the URL / URI endpoint
-
-.PARAMETER Url
-URL / URI for the D365FO environment you want to access through DMF
-
+﻿
+<#
+    .SYNOPSIS
+        Get DMF Package details
+        
+    .DESCRIPTION
+        Get all the needed details about a DMF package, so you can download it from the Dynamics 365 for Finance & Operations
+        
+    .PARAMETER JobId
+        The GUID from the recurring data job
+        
+    .PARAMETER AuthenticationToken
+        The token value that should be used to authenticate against the URL / URI endpoint
+        
+    .PARAMETER Url
+        URL / URI for the D365FO environment you want to access through DMF
+        
     .PARAMETER EnableException
         This parameters disables user-friendly warnings and enables the throwing of exceptions
         This is less user friendly, but allows catching exceptions in calling scripts
-
-.EXAMPLE
-PS C:\> Get-DmfPackageDetails -JobId "db5e719a-8db3-4fe5-9c78-7be479ce85a2" -Url "https://usnconeboxax1aos.cloud.onebox.dynamics.com" -AuthenticationToken "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOi....."
-
-This will fetch the available DMF package details.
-It will use "db5e719a-8db3-4fe5-9c78-7be479ce85a2" as the jobid parameter passed to the DMF endpoint.
-It will use "https://usnconeboxax1aos.cloud.onebox.dynamics.com" as the base D365FO environment url.
-It will use the "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOi....." as the bearer token for the endpoint.
-
-.NOTES
+        
+    .EXAMPLE
+        PS C:\> Get-DmfPackageDetails -JobId "db5e719a-8db3-4fe5-9c78-7be479ce85a2" -Url "https://usnconeboxax1aos.cloud.onebox.dynamics.com" -AuthenticationToken "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOi....."
+        
+        This will fetch the available DMF package details.
+        It will use "db5e719a-8db3-4fe5-9c78-7be479ce85a2" as the jobid parameter passed to the DMF endpoint.
+        It will use "https://usnconeboxax1aos.cloud.onebox.dynamics.com" as the base D365FO environment url.
+        It will use the "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOi....." as the bearer token for the endpoint.
+        
+    .NOTES
         Tags: Download, DMF, Package, Packages
         
         Author: Mötz Jensen (@Splaxi)
