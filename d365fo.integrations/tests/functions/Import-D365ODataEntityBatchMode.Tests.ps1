4382e163825b349c5bf14dae21efcd711c6f273f
Mötz Jensen <motz@e-s.dk>
2019-06-05 14:04:10 +0200

    Update Import-D365ODataEntityBatchMode.Tests.ps1

--- d365fo.integrations/tests/functions/Import-D365ODataEntityBatchMode.Tests.ps1
+++ d365fo.integrations/tests/functions/Import-D365ODataEntityBatchMode.Tests.ps1
@@ -102,6 +102,19 @@
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
 			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
 		}
+		It 'Should have the expected parameter RawOutput' {
+			$parameter = (Get-Command Import-D365ODataEntityBatchMode).Parameters['RawOutput']
+			$parameter.Name | Should -Be 'RawOutput'
+			$parameter.ParameterType.ToString() | Should -Be System.Management.Automation.SwitchParameter
+			$parameter.IsDynamic | Should -Be $False
+			$parameter.ParameterSets.Keys | Should -Be '__AllParameterSets'
+			$parameter.ParameterSets.Keys | Should -Contain '__AllParameterSets'
+			$parameter.ParameterSets['__AllParameterSets'].IsMandatory | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].Position | Should -Be -2147483648
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipeline | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromPipelineByPropertyName | Should -Be $False
+			$parameter.ParameterSets['__AllParameterSets'].ValueFromRemainingArguments | Should -Be $False
+		}
 		It 'Should have the expected parameter EnableException' {
 			$parameter = (Get-Command Import-D365ODataEntityBatchMode).Parameters['EnableException']
 			$parameter.Name | Should -Be 'EnableException'
@@ -120,7 +133,7 @@
 	Describe "Testing parameterset __AllParameterSets" {
 		<#
 		__AllParameterSets -EntityName -Payload
-		__AllParameterSets -EntityName -Payload -CrossCompany -Tenant -URL -ClientId -ClientSecret -EnableException
+		__AllParameterSets -EntityName -Payload -CrossCompany -Tenant -URL -ClientId -ClientSecret -RawOutput -EnableException
 		#>
 	}
 
