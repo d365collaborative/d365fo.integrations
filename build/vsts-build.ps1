ba685dd1f8cc0e7681656a7e96aaf0bc8b01a1e1
Mötz Jensen <motz@e-s.dk>
2019-06-10 19:22:40 +0200

    Implement better status during build / publish

--- build/vsts-build.ps1
+++ build/vsts-build.ps1
@@ -11,8 +11,10 @@ param (
 )
 
 # Prepare publish folder
-Write-PSFMessage -Level Important -Message "Creating and populating publishing directory"
+Write-PSFMessage -Level Important -Message "Creating and populating publishing directory."
 $publishDir = New-Item -Path $WorkingDirectory -Name publish -ItemType Directory
+
+Write-PSFMessage -Level Important -Message "Copying files from the working directory to the publish directory."
 Copy-Item -Path "$($WorkingDirectory)\d365fo.integrations" -Destination $publishDir.FullName -Recurse -Force
 
 #region Gather text data to compile
@@ -20,6 +22,7 @@ $text = @()
 $processed = @()
 
 # Gather Stuff to run before
+Write-PSFMessage -Level Important -Message "Fixing the before files."
 foreach ($line in (Get-Content "$($PSScriptRoot)\filesBefore.txt" | Where-Object { $_ -notlike "#*" }))
 {
 	if ([string]::IsNullOrWhiteSpace($line)) { continue }
@@ -36,14 +39,18 @@ foreach ($line in (Get-Content "$($PSScriptRoot)\filesBefore.txt" | Where-Object
 }
 
 # Gather commands
+Write-PSFMessage -Level Important -Message "Fetching internal functions."
 Get-ChildItem -Path "$($publishDir.FullName)\d365fo.integrations\internal\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
 	$text += [System.IO.File]::ReadAllText($_.FullName)
 }
+
+Write-PSFMessage -Level Important -Message "Fetching external functions."
 Get-ChildItem -Path "$($publishDir.FullName)\d365fo.integrations\functions\" -Recurse -File -Filter "*.ps1" | ForEach-Object {
 	$text += [System.IO.File]::ReadAllText($_.FullName)
 }
 
 # Gather stuff to run afterwards
+Write-PSFMessage -Level Important -Message "Fixing the after files."
 foreach ($line in (Get-Content "$($PSScriptRoot)\filesAfter.txt" | Where-Object { $_ -notlike "#*" }))
 {
 	if ([string]::IsNullOrWhiteSpace($line)) { continue }
@@ -61,6 +68,7 @@ foreach ($line in (Get-Content "$($PSScriptRoot)\filesAfter.txt" | Where-Object 
 #endregion Gather text data to compile
 
 #region Update the psm1 file
+Write-PSFMessage -Level Important -Message "Update the psm1 file."
 $fileData = Get-Content -Path "$($publishDir.FullName)\d365fo.integrations\d365fo.integrations.psm1" -Raw
 $fileData = $fileData.Replace('"<was not compiled>"', '"<was compiled>"')
 $fileData = $fileData.Replace('"<compile code into here>"', ($text -join "`n`n"))
@@ -68,4 +76,5 @@ $fileData = $fileData.Replace('"<compile code into here>"', ($text -join "`n`n")
 #endregion Update the psm1 file
 
 # Publish to Gallery
+Write-PSFMessage -Level Important -Message "Running the publish command against PowerShell gallery."
 Publish-Module -Path "$($publishDir.FullName)\d365fo.integrations" -NuGetApiKey $ApiKey -Force
