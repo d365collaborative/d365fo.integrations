aa35af2221a3cd14b77d5aa877bbff0d11604a44
Mötz Jensen <motz@e-s.dk>
2020-10-01 23:09:44 +0200

    Fix: Pester needs to be non-v5

--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -4,4 +4,5 @@
 # Needs to ensure things are Done Right and only legal commits to master get built
 
 # Run internal pester tests
+Import-Module -Name "Pester" -MaximumVersion 4.99.99 -Force
 & "$PSScriptRoot\..\d365fo.integrations\tests\pester.ps1"
