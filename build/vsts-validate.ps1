dec900e363112de3e20e89d25f41eef98519c451
Mötz Jensen <motz@e-s.dk>
2021-05-27 22:31:46 +0200

    Fix: Validate was missing param section.

--- build/vsts-validate.ps1
+++ build/vsts-validate.ps1
@@ -1,8 +1,33 @@
+param (
+    $TestGeneral = $true,
+	
+    $TestFunctions = $true,
+
+    $Exclude = ""
+
+)
+
 # Guide for available variables and working with secrets:
 # https://docs.microsoft.com/en-us/vsts/build-release/concepts/definitions/build/variables?tabs=powershell
 
 # Needs to ensure things are Done Right and only legal commits to master get built
 
 # Run internal pester tests
-Import-Module -Name "Pester" -MaximumVersion 4.99.99 -Force
-& "$PSScriptRoot\..\d365fo.integrations\tests\pester.ps1"
+
+Write-Host "Working on the machine named: $($env:computername)"
+Write-Host "The user running is: $($env:UserName)"
+
+$modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement")
+
+foreach ($item in $modules) {
+    $module = Get-Module -Name $item -ErrorAction SilentlyContinue
+
+    if ($null -eq $module) {
+        Write-Host "Importing $item" -ForegroundColor Cyan
+        Import-Module $item -Force
+    }
+}
+
+Import-Module "Pester" -MaximumVersion 4.99.99 -Force
+
+& "$PSScriptRoot\..\d365fo.integrations\tests\pester.ps1" -TestGeneral $TestGeneral -TestFunctions $TestFunctions -Exclude $Exclude
