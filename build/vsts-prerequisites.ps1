0c3f7b366fa90a1cbfb59728bf19a19bd92c3798
Mötz Jensen <motz@e-s.dk>
2021-05-27 22:15:24 +0200

    Feature: Refactor build scripts to make use of cache while building.

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,9 +1,18 @@
+Write-Host "Working on the machine named: $($env:computername)"
+Write-Host "The user running is: $($env:UserName)"
+
 $modules = @("PSFramework", "PSScriptAnalyzer", "Azure.Storage", "PSNotification", "PSOAuthHelper", "PowerShellGet", "PackageManagement")
 
-Install-Module "Pester" -MaximumVersion 4.99.99 -Force -SkipPublisherCheck -AllowClobber
+Install-Module "Pester" -MaximumVersion 4.99.99 -Force -Confirm:$false -Scope CurrentUser -AllowClobber -SkipPublisherCheck
 
-foreach ($module in $modules) {
-    Write-Host "Installing $module" -ForegroundColor Cyan
-    Install-Module $module -Force -SkipPublisherCheck -AllowClobber
-    Import-Module $module -Force -PassThru
+foreach ($item in $modules) {
+    
+    $module = Get-InstalledModule -Name $item -ErrorAction SilentlyContinue
+
+    if ($null -eq $module) {
+        Write-Host "Installing $item" -ForegroundColor Cyan
+        Install-Module -Name $item -Force -Confirm:$false -Scope CurrentUser -AllowClobber -SkipPublisherCheck
+    }
+
+    Import-Module $item -Force
 }
--- d365fo.integrations/tests/pester.ps1
+++ d365fo.integrations/tests/pester.ps1
@@ -28,16 +28,17 @@ $totalRun = 0
 $testresults = @()
 
 #region Run General Tests
-if ($TestGeneral)
-{
+if ($TestGeneral) {
 	Write-PSFMessage -Level Important -Message "Modules imported, proceeding with general tests"
-	foreach ($file in (Get-ChildItem "$PSScriptRoot\general" | Where-Object Name -like "*.Tests.ps1"))
-	{
+	foreach ($file in (Get-ChildItem "$PSScriptRoot\general" -Filter "*.Tests.ps1")) {
+	
+		if ($file.Name -notlike $Include) { continue }
+		if ($file.Name -like $Exclude) { continue }
+		
 		Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
 		$TestOuputFile = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).xml"
-    $results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
-		foreach ($result in $results)
-		{
+		$results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
+		foreach ($result in $results) {
 			$totalRun += $result.TotalCount
 			$totalFailed += $result.FailedCount
 			$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
@@ -45,7 +46,7 @@ if ($TestGeneral)
 				$testresults += [pscustomobject]@{
 					Describe = $_.Describe
 					Context  = $_.Context
-					Name	 = "It $name"
+					Name     = "It $name"
 					Result   = $_.Result
 					Message  = $_.FailureMessage
 				}
@@ -56,19 +57,16 @@ if ($TestGeneral)
 #endregion Run General Tests
 
 #region Test Commands
-if ($TestFunctions)
-{
-Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
-	foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File | Where-Object Name -like "*Tests.ps1"))
-	{
+if ($TestFunctions) {
+	Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
+	foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File -Filter "*Tests.ps1")) {
 		if ($file.Name -notlike $Include) { continue }
 		if ($file.Name -like $Exclude) { continue }
 		
 		Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
 		$TestOuputFile = Join-Path "$PSScriptRoot\..\..\TestResults" "TEST-$($file.BaseName).xml"
-    $results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
-		foreach ($result in $results)
-		{
+		$results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru -OutputFile $TestOuputFile -OutputFormat NUnitXml
+		foreach ($result in $results) {
 			$totalRun += $result.TotalCount
 			$totalFailed += $result.FailedCount
 			$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
@@ -76,7 +74,7 @@ Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
 				$testresults += [pscustomobject]@{
 					Describe = $_.Describe
 					Context  = $_.Context
-					Name	 = "It $name"
+					Name     = "It $name"
 					Result   = $_.Result
 					Message  = $_.FailureMessage
 				}
@@ -91,7 +89,6 @@ $testresults | Sort-Object Describe, Context, Name, Result, Message | Format-Lis
 if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
 else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
 
-if ($totalFailed -gt 0)
-{
+if ($totalFailed -gt 0) {
 	throw "$totalFailed / $totalRun tests failed!"
 }
